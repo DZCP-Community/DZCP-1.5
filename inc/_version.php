c5dda1b6bc70ae1f41dc60cda57b13e3bcf131b1
Lucas <godkiller_nt@hammermaps.de>
2015-01-16 19:54:26 +0100

    Init

---
+++ inc/_version.php
@@ -0,0 +1,8 @@
+<?php 
+  define('_version', '1.5.5.4');
+  define('_release', '06.05.2012');
+  
+  $cp_color = '#FFFFFF';  ###########################################################################
+                           # Hex Farbcode der Hintergrundfarbe Copyrightlinks am Ende der Homepage #
+                          ###########################################################################
+?>
